f90488729a05177089cfde452d9d4cb3da2da6f4
Edward Brash <brash@jlab.org>
2019-01-10 14:02:40 -0500

    Updates

--- charge_splitting.py
+++ charge_splitting.py
@@ -6,20 +6,21 @@ k = 8.99E+09
 
 force = [0.0 for i in range(0,1000)]
 q1 = [0.0 for i in range(0,1000)]
+q1uC = [0.0 for i in range(0,1000)]
 
 for i in range(0,1000):
     q1[i] = 1.0*i/999.0*qtot
     q2 = qtot - q1[i]
     force[i] = k*q1[i]*q2/(d*d)
-    print(i,q1[i],q2,force[i])
+    q1uC[i]=q1[i]*1.0E+06
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 
-ax1.plot(q1,force)
+ax1.plot(q1uC,force)
 
 ax1.set_title("Charge Splitting")
-ax1.set_xlabel('q1 (C)')
+ax1.set_xlabel('q1 (uC)')
 ax1.set_ylabel('Force (N)')
 ax1.grid(True)
 
