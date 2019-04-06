26f1ab5e2f5463fd3017d6713e7884eb9d2020f5
Edward Brash <brash@jlab.org>
2019-04-06 10:48:27 -0400

    Update fitting function

--- basic_fit_xkcd.py
+++ basic_fit_xkcd.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
+from scipy import signal
 
-def fitfunction(x,a,b,c,d,e,f,g,h,i):
-    return e*np.sin(d*x+f) + g*np.sin(h*x*x+i) + a*x*x + b*x + c
+#def fitfunction(x,a,b,c,d,e,f,g,h,i):
+#    return e*np.sin(d*x+f) + g*np.sin(h*x*x+i) + a*x*x + b*x + c
+def fitfunction(x,a,b,c,d):
+    return  d*signal.sawtooth(0.2*(x-7.5)) + a*x*x + b*x + c
 
 x = []
 y = []
 
 x, y = np.loadtxt('example2.dat', delimiter=',', unpack=True)
+xfit = np.linspace(0.5,15.5,num=500)
 
 popt,pcov = curve_fit(fitfunction,x,y)
 perr = np.sqrt(np.diag(pcov))
